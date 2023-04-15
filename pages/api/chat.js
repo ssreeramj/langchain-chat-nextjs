2e9ebbc12e20b93cd4170a123a5b9ebd810cc4cd
Sreeram Jagannath <ssreeramj@gmail.com>
2023-04-15 19:01:28 +0000

    modified code

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,18 +1,37 @@
+// export default async function (req, res) {
+
+  // const response = await fetch(process.env.LCC_ENDPOINT_URL, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "X-Api-Key": process.env.LCC_TOKEN
+  //   },
+  //   body: JSON.stringify({
+  //     question: req.body.question,
+  //     history: req.body.history
+  //   }),
+  // });
+
+  //   const data = await response.json();
+
+//     const data = {
+//       success: "This is a dummy success message",
+//       // Other properties as needed
+//     }
+
+//     res.status(200).json({ result: data })
+// }
+
+
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+
+const chat = new ChatOpenAI({ temperature: 0, openAIApiKey: "" });
+
 export default async function (req, res) {
-
-  const response = await fetch(process.env.LCC_ENDPOINT_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Api-Key": process.env.LCC_TOKEN
-    },
-    body: JSON.stringify({
-      question: req.body.question,
-      history: req.body.history
-    }),
-  });
-
-    const data = await response.json();
-
-    res.status(200).json({ result: data })
+  const response = await chat.call([
+    new HumanChatMessage(req.body.question),
+  ]);
+  
+  console.log(response);
 }
--- pages/index.js
+++ pages/index.js
@@ -98,18 +98,18 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>LangChain Chat</title>
+        <title>Talk to a Video</title>
         <meta name="description" content="LangChain documentation chatbot" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.topnav}>
       <div className = {styles.navlogo}>
-    <a href="/">LangChain</a>
+    <a href="/">Talk to a Video</a>
     </div>
     <div className = {styles.navlinks}>
-    <a href="https://langchain.readthedocs.io/en/latest/" target="_blank">Docs</a>
-    <a href="https://github.com/zahidkhawaja/langchain-chat-nextjs" target="_blank">GitHub</a>
+    {/* <a href="https://langchain.readthedocs.io/en/latest/" target="_blank">Docs</a> */}
+    {/* <a href="https://github.com/zahidkhawaja/langchain-chat-nextjs" target="_blank">GitHub</a> */}
     </div>
 </div>
       <main className={styles.main}>
@@ -163,7 +163,7 @@ export default function Home() {
             </form>
             </div>
             <div className = {styles.footer}>
-            <p>Powered by <a href = "https://github.com/hwchase17/langchain" target="_blank">LangChain</a>. Built by <a href="https://twitter.com/chillzaza_" target="_blank">Zahid</a>.</p>
+            {/* <p>Powered by <a href = "https://github.com/hwchase17/langchain" target="_blank">LangChain</a>. Built by <a href="https://twitter.com/chillzaza_" target="_blank">Zahid</a>.</p> */}
             </div>
         </div>
       </main>
